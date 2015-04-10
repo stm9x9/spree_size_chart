8218577321e18922d332e14a03e03cacb188869c
Nick Chubarov <nick.chubarov@gmail.com>
2015-04-10 10:38:08 +0400

    Strong parameters

--- app/controllers/spree/admin/size_charts_controller.rb
+++ app/controllers/spree/admin/size_charts_controller.rb
@@ -1,14 +1,14 @@
 module Spree
   module Admin
     class SizeChartsController < ResourceController
-      belongs_to 'spree/product', :find_by => :permalink
+      belongs_to 'spree/product', :find_by => :slug
 
       def edit
       end
 
       def update
-        if @size_chart.update_attributes params[:size_chart]
-          flash[:notice] = "Size Chart successfully updated"
+        if @size_chart.update_attributes permitted_resource_params
+          flash[:notice] = 'Size Chart successfully updated'
         else
         end
         redirect_to edit_admin_product_size_chart_path(parent)
@@ -20,6 +20,13 @@ module Spree
 
       def load_resource_instance
         @size_chart ||= parent.size_chart || parent.build_size_chart
+      end
+
+
+      private
+
+      def permitted_resource_params
+        params.require(:size_chart).permit(:id, :name, :product_id, :option_type_id, :unit, :prototype_id)
       end
     end
   end
--- app/controllers/spree/admin/size_types_controller.rb
+++ app/controllers/spree/admin/size_types_controller.rb
@@ -7,6 +7,13 @@ module Spree
           format.json { render :json => json_data }
         end
       end
+
+      private
+
+      def permitted_resource_params
+        params.require(:size_type).permit(:id, :name)
+      end
+
     end
   end
 end
